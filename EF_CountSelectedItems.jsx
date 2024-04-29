f9ba33f7d9cc5142a691c0a914238107ee823073
Eveline Laptop <falcao.eveline@gmail.com>
2024-04-28 21:15:41 -0300

    add CountSelectedItems

---
+++ EF_CountSelectedItems.jsx
@@ -0,0 +1,21 @@
+/**========================================================================
+ * ?                     EF_CountSelectedItems.jsx
+ * @author         :  Eveline Falcão (https://evelinefalcao.com)
+ * @email          :  hello@evelinefalcao.com
+ * @version        :  1.0.0
+ * @createdFor     :  Adobe After Effects CC 2024 (Version 24.1.0 Build 78)
+ * @description    :  Counts the selected items in the Project Panel and outputs the result to the Info Panel.
+ *========================================================================**/
+
+(function countSelectedProjectItems(){
+
+    var selectedItems = app.project.selection;
+    
+    clearOutput();
+
+    if(selectedItems != null){
+        writeLn("Selected items: " + selectedItems.length);
+    } else {
+        alert("Select some project items to continue.")
+    }
+})();
