dab3c60e0797649253a233103432ff0f2f10bf02
Eveline Falcão <falcao.eveline@gmail.com>
2024-06-22 09:25:26 -0300

    add padWithZeros and error handling

--- EF_SaveFrameToPNG.jsx
+++ EF_SaveFrameToPNG.jsx
@@ -13,12 +13,30 @@
         return Math.round(currentTime * frameRate);
     }
 
+    function padWithZeros(number, digits) {
+        var str = number.toString()
+        while (str.length < digits) {
+            str = '0' + str;
+        }
+        return str;
+    }
+
     var comp = app.project.activeItem;
-    var projectPath = app.project.file.toString().replace(".aep", "");
-    var currentTime = comp.time;
-    var frameRate = comp.frameRate;
-    var currentFrame = currentTimeToFrames(currentTime, frameRate).toFixed();
 
-    comp.saveFrameToPng(currentTime, File(projectPath + "_00" + currentFrame + ".png"));
+    if(comp instanceof CompItem && comp != null) {
+        var currentTime = comp.time;
+        var frameRate = comp.frameRate;
+        var currentFrame = currentTimeToFrames(currentTime, frameRate);
+        var projectPath = app.project.file;
 
+        if(projectPath != null){
+            var stringProjPath = projectPath.toString().replace(".aep", "");
+            comp.saveFrameToPng(currentTime, File(stringProjPath + "_" + padWithZeros(currentFrame, 6) + ".png"));
+        } else {
+            alert("Save your project to continue.")
+        }
+        
+    } else {
+        alert("Select your active Comp in the project panel or timeline.");
+    }
 })();
