724ffcda05d22b8682276e780372f36615637112
Eveline Falcão <falcao.eveline@gmail.com>
2024-09-15 11:59:13 -0300

    fix identation on an if statement

--- EF_ExportMissingFootagesPathsToFile.jsx
+++ EF_ExportMissingFootagesPathsToFile.jsx
@@ -14,9 +14,9 @@
 
     // Pushes every missing footage to the missingItems Array
     for(var item = 1; item <= projectItems.length; item++){
-    if(projectItems[item].footageMissing){
-        var footagePath = projectItems[item].mainSource.missingFootagePath;
-        missingItems.push(footagePath);
+        if(projectItems[item].footageMissing){
+            var footagePath = projectItems[item].mainSource.missingFootagePath;
+            missingItems.push(footagePath);
         }
     }  
 
