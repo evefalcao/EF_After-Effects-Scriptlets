8a995072d31903bb521ab5455d7b679f7c999527
Eveline Falcão <falcao.eveline@gmail.com>
2024-10-07 11:29:00 -0300

    remove an unused expression, fix header

--- EF_AddPosterizeTimeZero.jsx
+++ EF_AddPosterizeTimeZero.jsx
@@ -1,15 +1,15 @@
 /**========================================================================
  * ?                  EF_AddPosterizeTimeZero.jsx
- * @author         :  Eveline Falcão (https://evelinefalcao.com)
- * @email          :  hello@evelinefalcao.com
- * @version        :  1.0.0
- * @createdFor     :  Adobe After Effects CC 2024 (Version 24.1.0 Build 78)
- * @description    :  Add "PosterizeTime(0);" to the beginning of all the selected properties expressions.
+ * @author            Eveline Falcão <https://evelinefalcao.com>
+ * @email             hello@evelinefalcao.com
+ * @version           1.0.0
+ * @createdFor        Adobe After Effects CC 2024 (Version 24.1.0 Build 78)
+ * @description       Add "PosterizeTime(0);" to the beginning of all the selected properties expressions.
  *========================================================================**/
 
 (function addPosterizeTimeZero(){
 
-    function addPosterizeExpression(property, expression){
+    function addPosterizeExpression(property){
         var propertyExpression = property.expression;
         // if the property has no keyframes or animation driven by expression, apply posterizeTime(0), else leave as is
         if(!property.numKeys){
