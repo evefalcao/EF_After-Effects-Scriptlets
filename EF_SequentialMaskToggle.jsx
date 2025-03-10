93cb2dc9c08ea33df1eaaf1821a62ae038e7e79b
Eveline Falcão <falcao.eveline@gmail.com>
2025-03-10 15:43:30 -0300

    (fix) fix script to work with selected layers only

--- EF_SequentialMaskToggle.jsx
+++ EF_SequentialMaskToggle.jsx
@@ -13,25 +13,32 @@
   var comp = app.project.activeItem;
   var layers = comp.selectedLayers;
 
+  function findIndexInArray(item) {
+    for (var i = 0; i < layers.length; i++) {
+        if (layers[i] === item) {
+            return i;
+        }
+    }
+    return -1;
+  }
+
   app.beginUndoGroup("'Sequential Mask Toggle'");
 
   for (var layer = 0; layer < layers.length; layer++) {
 
-      var curLayer = layers[layer];
-      var curLayerIndex = curLayer.index;
-      var maskTotal = curLayer.mask.numProperties
+    var curLayer = layers[layer];
+    var curLayerIndexInSelection = findIndexInArray(layers[layer]);
+    var maskTotal = curLayer.mask.numProperties;
 
-      for (var maskIndex = 1; maskIndex <= maskTotal; maskIndex++) {
+    for (var maskIndex = 1; maskIndex <= maskTotal; maskIndex++) {
+      var curMask = curLayer.mask(maskIndex);
 
-        var curMask = curLayer.mask(maskIndex);
-
-        if (curMask.propertyIndex === curLayerIndex) {
-          curMask.maskMode = MaskMode.ADD;
-        } else {
-          curMask.maskMode = MaskMode.NONE;
-        }
-
+      if (curMask.propertyIndex === curLayerIndexInSelection + 1) {
+        curMask.maskMode = MaskMode.ADD;
+      } else {
+        curMask.maskMode = MaskMode.NONE;
       }
+    }
   }
 
   app.endUndoGroup();
