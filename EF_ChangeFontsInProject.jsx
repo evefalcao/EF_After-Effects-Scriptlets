6c9b70cdafc8e3e44faa141248402ba7ece1b7fd
Eveline Falcão <falcao.eveline@gmail.com>
2025-10-03 11:43:22 -0300

    edit function calle

--- EF_ChangeFontsInProject.jsx
+++ EF_ChangeFontsInProject.jsx
@@ -7,39 +7,43 @@
  * @description    :  Changes the font and style of text layers of the entire project.
  *========================================================================**/
 
-var newFont = "mountain_king"; // Use the postscript name as the font name
-var newStyle = "Regular";
+(function ChangeFontsInProject() {
+    var newFont = "CSAriston-Regular"; // Use the postscript name as the font name
+    var newStyle = "Regular";
 
-// Check the actual font name
-// var textProp = app.project.activeItem.selectedLayers[0].property("Source Text");
-// var textDocument = textProp.value;
-// alert(textDocument.font);
+    // Check the actual font name
+    // var textProp = app.project.activeItem.selectedLayers[0].property("Source Text");
+    // var textDocument = textProp.value;
+    // alert(textDocument.font);
 
-function changeFontsInTextLayer(textLayer, fontName) {
-    var textProp = textLayer.property("Source Text");
-    var textDocument = textProp.value;
-    textDocument.font = fontName;
-    textProp.setValue(textDocument);
-}
+    function changeFontsInTextLayer(textLayer, fontName) {
+        var textProp = textLayer.property("Source Text");
+        var textDocument = textProp.value;
+        textDocument.font = fontName;
+        textProp.setValue(textDocument);
+    }
 
-function processComp(comp, fontName) {
-    for (var i = 1; i <= comp.numLayers; i++) {
-        var layer = comp.layer(i);
-        if (layer instanceof TextLayer) {
-            changeFontsInTextLayer(layer, fontName);
-        } else if (layer instanceof AVLayer && layer.source instanceof CompItem) {
-            processComp(layer.source, fontName);
+    function processComp(comp, fontName) {
+        for (var i = 1; i <= comp.numLayers; i++) {
+            var layer = comp.layer(i);
+            if (layer instanceof TextLayer) {
+                changeFontsInTextLayer(layer, fontName);
+            } else if (layer instanceof AVLayer && layer.source instanceof CompItem) {
+                processComp(layer.source, fontName);
+            }
         }
     }
-}
 
-app.beginUndoGroup("Batch Change Fonts");
+    app.beginUndoGroup("Batch Change Fonts");
 
-for (var i = 1; i <= app.project.numItems; i++) {
-    var item = app.project.item(i);
-    if (item instanceof CompItem) {
-        processComp(item, newFont, newStyle);
+    for (var i = 1; i <= app.project.numItems; i++) {
+        var item = app.project.item(i);
+        if (item instanceof CompItem) {
+            processComp(item, newFont, newStyle);
+        }
     }
-}
 
-app.endUndoGroup();
+    app.endUndoGroup();
+
+})();
+
